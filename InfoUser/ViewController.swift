1dd46b79e25d52f813f251ac3bdab832c4b8c6b0
Spote777 <66115092+Spote777@users.noreply.github.com>
2021-06-11 12:15:03 +0300

    Initial commit

---
+++ InfoUser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InfoUser
+//
+//  Created by Павел Заруцков on 11.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
